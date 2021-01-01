e1d2171d518ea80a91b9ca92e88f0f8b4d396116
PC-ANTONIO\toni_ <antonioolvera1995@gmail.com>
2021-01-01 17:48:05 +0100

    *Structure basic and home*

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./core/Home";
+import Footer from "./shared/footer";
+import Header from "./shared/header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="main" style={{height: height()}}>
+      <Header />
+      <Home />
+      <Footer />
+    </main>
   );
+}
+
+function height() {
+  let heightt = window.innerHeight;
+  heightt = heightt -500;
+  return 0;
 }
 
 export default App;
---
+++ src/core/Home.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <section className="section-home">
+        <div className="title-home">
+          <h1>Welcome to my React practice</h1>
+          <p>
+        Here you will find several examples of  the uses <br/>you can give React
+        </p>
+        </div>
+        
+      </section>
+    );
+  }
+}
+
+export default Home;
